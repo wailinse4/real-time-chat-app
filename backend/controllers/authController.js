5f08999c36aaeb6714f204821882c7900f8e3e85
wailinse4 <wailinse4@gmail.com>
2025-01-29 13:09:19 -0500

    feat: Add logout functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -72,5 +72,11 @@ export const login = async (req, res) => {
 }
 
 export const logout = async (req, res) => {
-    res.send("Logout")
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Logout Successful" })
+    }
+    catch(error) {
+        res.status(500).json({ message: "Internal Server Error" }) 
+    }
 }
