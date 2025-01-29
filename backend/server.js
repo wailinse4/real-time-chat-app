42edab5be815ce36f8db09223cae699553984532
wailinse4 <wailinse4@gmail.com>
2025-01-28 23:09:38 -0500

    feat: Add body parsing middleware

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.use("/api/auth", authRoutes)
 
 connectDB()
